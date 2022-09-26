16c8529ee736c4b15686029997d070a2c71c4bb7
michael <m@ciccarel.li>
2022-09-26 10:55:25 -0400

    swap font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,41 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
     fontFamily: {
-      body: ['Maison Neue', ...defaultTheme.fontFamily.sans],
-      mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
+      // body: ['Maison Neue', ...defaultTheme.fontFamily.sans],
+      body: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+      mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
     },
     extend: {
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        success: "#0070f3",
+        cyan: "#79FFE1",
       },
       spacing: {
-        28: '7rem',
+        28: "7rem",
       },
       letterSpacing: {
-        tighter: '-.04em',
+        tighter: "-.04em",
       },
       lineHeight: {
         tight: 1.2,
       },
       fontSize: {
-        xs: '.625rem',
-        sm: '.6875rem',
-        base: '.75rem',
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
+        xs: ".625rem",
+        sm: ".6875rem",
+        base: ".75rem",
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
       },
       boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
+        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
     },
   },
